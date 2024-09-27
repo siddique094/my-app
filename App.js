34efb9b4c44f1dadc1941f965b1f7248d2878f98
siddique094 <109813408+siddique094@users.noreply.github.com>
2024-09-27 05:00:01 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import Navigation from './src/components/Navigation'
+
+const App = () => {
+      return <Navigation />
+  };
+export default App;
